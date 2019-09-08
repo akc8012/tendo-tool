51bc277a115c7362afda54379884521d7ffe756e
Andrew Colannino <akc8012@gmail.com>
2019-09-08 17:29:15 -0400

    working make_file module (yeet)

---
+++ src/make_file.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn make(path: &str, text: &str) {
+	let path = Path::new(path);
+
+	let file = create_file(path);
+	write_to_file(path, file, text);
+}
+
+fn create_file(path: &std::path::Path) -> std::fs::File {
+	match File::create(&path) {
+		Err(why) => panic!("Couldn't create {}: {}", path.display(), why.description()),
+		Ok(file) => file,
+	}
+}
+
+fn write_to_file(path: &std::path::Path, mut file: std::fs::File, text: &str) {
+	match file.write_all(text.as_bytes()) {
+		Err(why) => panic!(
+			"Couldn't write to {}: {}",
+			path.display(),
+			why.description()
+		),
+		Ok(_) => println!("Successfully wrote to {}", path.display()),
+	}
+}
