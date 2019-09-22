61e6e16ed0093ae5a3864fbdc325828f6ba4d410
Andrew Colannino <akc8012@gmail.com>
2019-09-22 16:28:29 -0400

    Parsed template into dictionary

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 import os
 
 script_path = os.path.dirname(__file__)
-template_path = "../assets/template.md"
+template_path = '../assets/template.md'
 
 path = os.path.join(script_path, template_path)
-file = open(path)
 
-print(file.read())
+dict = {}
+count = 0
+dash_line_count = 0
+
+with open(path) as file:
+	for line in file:
+		if line.strip() == '---':
+			dash_line_count += 1
+			if dash_line_count == 1:
+				continue
+			elif dash_line_count == 2:
+				break
+
+		if count == 0:
+			dict[line.strip()] = 'default'
+		else:
+			dict[line.strip()] = ''
+
+		# print(line)
+		count += 1
+
+print(dict)
