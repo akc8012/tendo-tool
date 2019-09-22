6b529ea86834808a691331847b07159306b05d68
Andrew Colannino <akc8012@gmail.com>
2019-09-22 14:42:39 -0400

    no more rust stuff (bye bye!)

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-mod make_file;
-
-fn main() {
-	let arguments: Vec<String> = env::args().collect();
-	println!("{:?}", arguments);
-
-	make_file::make("out/yeet.txt", "yeet to the max!");
-}
