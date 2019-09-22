cde53243a1eec5b60b5f3df8b7bad6bd64c1652e
Andrew Colannino <akc8012@gmail.com>
2019-09-22 16:38:04 -0400

    lets DO IT [pull in dictionary values]

--- src/main.py
+++ src/main.py
@@ -5,25 +5,23 @@ template_path = '../assets/template.md'
 
 path = os.path.join(script_path, template_path)
 
-dict = {}
-count = 0
+front_matter = {}
 dash_line_count = 0
 
 with open(path) as file:
 	for line in file:
-		if line.strip() == '---':
+		line = line.strip()
+		if line == '---':
 			dash_line_count += 1
 			if dash_line_count == 1:
 				continue
 			elif dash_line_count == 2:
 				break
 
-		if count == 0:
-			dict[line.strip()] = 'default'
-		else:
-			dict[line.strip()] = ''
+		key, value = line.split(':')
+		key = key.strip()
+		value = value.strip()
 
-		# print(line)
-		count += 1
+		front_matter[key] = value
 
-print(dict)
+print(front_matter)
